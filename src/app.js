989ce49acc035f9b8f848805d81d69cdcbe554f5
rajeshit1421 <84392282+rajeshit1421@users.noreply.github.com>
2023-03-31 20:52:00 +0530

    Sub website tut (#5)
    * routes and template engine ejs
    
    * add connection with database and create schema
    
    * resolve conflicts
    
    * form post and save to database

--- src/app.js
+++ src/app.js
@@ -1,57 +1,59 @@
-
 const dotenv = require("dotenv");
-dotenv.config(); 
+dotenv.config();
 const express = require("express");
-const logger = require('./logger');
-const routes = require('./routes/main');
+const logger = require("./logger");
+const routes = require("./routes/main");
 const hbs = require("hbs");
+const bodyParser = require("body-parser");
+const Detail = require("./models/detail");
 
-const Detail = require("./models/detail")
 const app = express();
-const mongoose =require("mongoose");
-const port = process.env.PORT||3000;
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
 
-app.use('/static',express.static("public"));
-app.use('/',routes);
-
+app.use(express.urlencoded({ extended: true }));
+app.use("/static", express.static("public"));
+app.use("/", routes);
 //Template engine
-app.set('view engine','hbs');
-app.set("views","views");
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-hbs.registerPartials("views/partial")
+hbs.registerPartials("views/partial");
 
 //db connections
 mongoose.connect("mongodb://127.0.0.1:27017/website_tut", {
   useNewUrlParser: "true",
-})
+});
 const db = mongoose.connection;
-db.on("error", err => {
-  console.log("err", err)
-})
+db.on("error", (err) => {
+  console.log("err", err);
+});
 db.on("connected", (err, res) => {
-//   Detail.create({
-//     brandName:"R V Technologogy",
-//     brandIconurl:"",
-//     links:[
-//         {
-//         label:"Home",
-//         url:"/"
-//     }, {
-//       label:"Services",
-//       url:"/services"
-//   },{
-//     label:"Gallery",
-//     url:"/gallery"
-// },{
-//   label:"About",
-//   url:"/about"
-// },{
-//   label:"Contact Us",
-//   url:"/contact-us"
-// }
-// ],
-//   })
-})
-app.listen(port,()=>{
-    console.log(`Server estblished ${port}`);
-})
+
+  //   Detail.create({
+  //     brandName:"R V Technologogy",
+  //     brandIconurl:"",
+  //     links:[
+  //         {
+  //         label:"Home",
+  //         url:"/"
+  //     }, {
+  //       label:"Services",
+  //       url:"/services"
+  //   },{
+  //     label:"Gallery",
+  //     url:"/gallery"
+  // },{
+  //   label:"About",
+  //   url:"/about"
+  // },{
+  //   label:"Contact Us",
+  //   url:"/contact-us"
+  // }
+  // ],
+  //   })
+});
+app.listen(port, () => {
+  console.log(`Server estblished ${port}`);
+});
+

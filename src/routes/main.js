acfd4c891ca84627fc3f75346c5713b946f8c2d8
rajeshit1421 <84392282+rajeshit1421@users.noreply.github.com>
2023-03-31 19:12:23 +0530

    Sub website tut (#3)
    * routes and template engine ejs
    
    * add connection with database and create schema

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const routes = express.Router();
 
-routes.get("/",(req,res)=>{
-    res.render("index");
+const Detail = require("../models/detail");
+
+routes.get("/",async(req,res)=>{
+  const details = await Detail.findOne({"_id":"6426dd87f2b092256e30901f"});  
+  res.render("index",{
+    details:details
+  })
 })
-routes.get("/gallery",(req,res)=>{
-    res.render("gallery");
-})
+routes.get("/gallery",async(req,res)=>{
+    const details = await Detail.findOne({"_id":"6426dd87f2b092256e30901f"});  
+    res.render("gallery",{
+      details:details
+    })
+   
+
 module.exports = routes;

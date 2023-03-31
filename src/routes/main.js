633790a2a111613e8a37b195be90cb300789cacf
Rajesh Vishwakarma <rajeshit1421@gmail.com>
2023-03-31 20:49:33 +0530

    form post and save to database

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,18 +2,26 @@ const express = require("express");
 const routes = express.Router();
 
 const Detail = require("../models/detail");
+const Contact = require("../models/contact");
 
-routes.get("/",async(req,res)=>{
-  const details = await Detail.findOne({"_id":"6426dd87f2b092256e30901f"});  
-  res.render("index",{
-    details:details
-  })
-})
-routes.get("/gallery",async(req,res)=>{
-    const details = await Detail.findOne({"_id":"6426dd87f2b092256e30901f"});  
-    res.render("gallery",{
-      details:details
-    })
+routes.get("/", async (req, res) => {
+  const details = await Detail.findOne({ _id: "6426dd87f2b092256e30901f" });
+  res.render("index", {
+    details: details,
   });
-
+});
+routes.get("/gallery", async (req, res) => {
+  const details = await Detail.findOne({ _id: "6426dd87f2b092256e30901f" });
+  res.render("gallery", {
+    details: details,
+  });
+});
+routes.post("/process-contact-form", async (req, res) => {
+  try {
+    const data = Contact.create(req.body);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+});
 module.exports = routes;
